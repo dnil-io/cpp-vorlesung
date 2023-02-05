382a4e8d69bf6d6c97a2178f3acc98bf9f2a884b
Conner <zargor3@gmail.com>
2023-02-05 16:56:00 +0100

    updates

--- include/CNumber.h
+++ include/CNumber.h
@@ -1,21 +1,32 @@
 #ifndef CNUMBER_H
 #define CNUMBER_H
 
+#include <optional>
+
 struct PolarCoordinates {
-  double r, phi;
+    double r= 0, phi = 0;
 };
 
 struct CartesianCoordinates {
-  double a, b;
+    double x = 0, y = 0;
 };
 
 class CNumber {
-  public:    
-    void setCartesian(CartesianCoordinates *coordinates);
-    void setPolar(PolarCoordinates *coordinates);
-    PolarCoordinates getPolar();
-    CartesianCoordinates getCartesian();
-  private:        
-    double a, b;
+public:
+    void setCartesian(std::optional<double> x, std::optional<double> y);
+
+    void setPolar(std::optional<double> r, std::optional<double> phi);
+
+    PolarCoordinates getAsPolar();
+
+    CartesianCoordinates getAsCartesian();
+
+private:
+    PolarCoordinates polarCoordinates = PolarCoordinates {};
+    CartesianCoordinates cartesianCoordinates = CartesianCoordinates {};
+
+    static PolarCoordinates fromCartesian(CartesianCoordinates &coords);
+    static CartesianCoordinates fromPolar(PolarCoordinates &coords);
 };
+
 #endif
