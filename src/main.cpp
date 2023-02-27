4e1577aab39a52cdcb1054bf6e0eb20258ce59c4
Daniel <dotonesimple@gmail.com>
2023-02-27 09:21:22 +0100

    almost done but i have to work now

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,36 @@
 #include "Kreis.h"
+#include "Rechteck.h"
 #include "Koordinate.h"
 #include <iostream>
 
+int readInt(std::string prompt) {
+    std::string input_line;
+    std::cout << prompt << "?: ";
+    std::getline(std::cin, input_line);
+    return std::stoi(input_line);
+}
 
+Koordinate readKoordinate() {
+    return Koordinate(readInt("x"), readInt("y"));
+}
 
 int main() {
     std::string input_line;
     while(std::cin) {
         std::cout << "input (kreis/rechteck)?";
         std::getline(std::cin, input_line);
+        ZeichenElement& el;
         if(input_line == "kreis") {
-            std::cout << "x";
-            std::getline(std::cin, input_line);
+            el = Kreis(readKoordinate(), readInt("radius"));
         } else if(input_line == "rechteck") {
+            el = Rechteck(readKoordinate(), readInt("height"), readInt("width"));
 
         } else {
             std::cout << "try again" << std::endl;
+            continue;
         }
     }
     Kreis k = Kreis();
     ZeichenElement& kreis = k;
     std::cout << "hi" << k.getKoordinate().getX() << k.getCircumrefrence() << std::endl;
-}
-
-Koordinate readKoordinate() {
-    std::string input_line;
-    std::cout << "x?";
-    std::getline(std::cin, input_line);
-
 }
